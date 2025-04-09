7516311d8d908234396a0a386db077370eaae52b
Saya Kim-Suzuki <ssolbeeks@gmail.com>
2025-04-08 18:23:03 -0700

    fix tests?

--- run_tests.sh
+++ run_tests.sh
@@ -4,19 +4,12 @@ failed=false
 
 mkdir -p results
 
-HOST=localhost
-PORT=1048
-USER=postgres
-DB=pg_normalized
-PASSWORD=pass
-export PGPASSWORD=$PASSWORD
-
 for problem in sql/*; do
     printf "$problem "
     problem_id=$(basename ${problem%.sql})
     result="results/$problem_id.out"
     expected="expected/$problem_id.out"
-    psql -h $HOST -p $PORT -U $USER -d $DB < $problem > $result
+    psql -U postgres -d pg_normalized < $problem > $result
     DIFF=$(diff -B $expected $result)
     if [ -z "$DIFF" ]; then
         echo pass
